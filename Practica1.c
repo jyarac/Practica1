b47d57ee6278910c72c32cbbda82f464df5629dc
dmirandam <dmirandam@unal.edu.co>
2023-04-16 21:52:02 -0500

    imports

--- Practica1.c
+++ Practica1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash.h"
+#include "leer.h"
 #include <unistd.h>
 // Definición de la estructura de datos para almacenar la información de tiempos de viaje
 
