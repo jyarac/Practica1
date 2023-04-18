6955766c9583be69ba8d4da1128409b9537dd972
jyarac <jyarac@unal.edu.co>
2023-04-17 23:19:56 -0500

    YO SOY EL CAMINO, LA VERDAD Y LA VIDA. Nadie viene al Padre, sino por mí. (Juan 14:6)

--- hash.h
+++ hash.h
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
+#define MAX_LINE_LENGTH 5024
+#define HASH_SIZE 1160
 // Definición de la estructura de un nodo
 typedef struct Nodo {
     int sourceid;
     int dstid;
     int hod;
     float mean_travel_time;
+    struct Nodo* siguiente;
 } Nodo;
+
+// Definición de la estructura de la tabla hash
+typedef struct HashTable {
+    int tamano;
+    Nodo** tabla;
+} HashTable;
+
+// Función hash para calcular el índice
+int funcion_hash(int sourceid, int dstid, int hod, int tamano) {
+    return (((sourceid * 31) ^ dstid) * 17 ^ hod) % tamano;
+}
 
 // Función para crear un nuevo nodo
 Nodo* crear_nodo(int sourceid, int dstid, int hod, float mean_travel_time) {
@@ -18,11 +30,60 @@ Nodo* crear_nodo(int sourceid, int dstid, int hod, float mean_travel_time) {
     nodo->dstid = dstid;
     nodo->hod = hod;
     nodo->mean_travel_time = mean_travel_time;
+    nodo->siguiente = NULL;
     return nodo;
 }
 
-// Función principal
-int hashDocument() {
+// Función para insertar un nodo en la tabla hash
+void insertar(HashTable* tabla, int sourceid, int dstid, int hod, float mean_travel_time) {
+    // Calcular el índice de la tabla hash
+    int indice = funcion_hash(sourceid, dstid, hod, tabla->tamano);
+
+    // Crear un nuevo nodo
+    Nodo* nodo = crear_nodo(sourceid, dstid, hod, mean_travel_time);
+
+    // Insertar el nodo en la tabla hash
+    if (tabla->tabla[indice] == NULL) {
+        tabla->tabla[indice] = nodo;
+    } else {
+        Nodo* actual = tabla->tabla[indice];
+        while (actual->siguiente != NULL) {
+            actual = actual->siguiente;
+        }
+        actual->siguiente = nodo;
+    }
+}
+
+// Función para buscar un nodo en la tabla hash
+Nodo* buscar(HashTable* tabla, int sourceid, int dstid, int hod) {
+    // Calcular el índice de la tabla hash
+    int indice = funcion_hash(sourceid, dstid, hod, tabla->tamano);
+
+    // Buscar el nodo en la tabla hash
+    Nodo* actual = tabla->tabla[indice];
+    while (actual != NULL) {
+        if (actual->sourceid == sourceid && actual->dstid == dstid && actual->hod == hod) {
+            return actual;
+        }
+        actual = actual->siguiente;
+    }
+    return NULL;
+}
+
+
+// Función para buscar un nodo en el archivo binario
+Nodo* buscar_binario(FILE* binario, int sourceid, int dstid, int hod) {
+    // Buscar el nodo en el archivo binario
+    Nodo* nodo = (Nodo*)malloc(sizeof(Nodo));
+    while (fread(nodo, sizeof(Nodo), 1, binario) != 0) {
+        if (nodo->sourceid == sourceid && nodo->dstid == dstid && nodo->hod == hod) {
+            return nodo;
+        }
+    }
+    free(nodo);
+    return NULL;
+}
+int hashDocument(){
     // Leer el archivo CSV
     FILE* archivo = fopen("datos.csv", "r");
     if (archivo == NULL) {
@@ -31,47 +92,55 @@ int hashDocument() {
     }
 
     // Abrir el archivo binario para escritura
-    FILE* archivo_binario = fopen("binario.bin", "wb");
-    if (archivo_binario == NULL) {
+    FILE* binario = fopen("binario.bin", "wb");
+    if (binario == NULL) {
         printf("Error al abrir el archivo binario\n");
         return 1;
     }
 
-    // Leer cada línea del archivo CSV y escribirla en el archivo binario
+    // Leer cada línea del archivo CSV y guardarla en el archivo binario
     char linea[1024];
     while (fgets(linea, 1024, archivo)) {
         int sourceid, dstid, hod;
         float mean_travel_time;
         sscanf(linea, "%d,%d,%d,%f", &sourceid, &dstid, &hod, &mean_travel_time);
+
+        // Crear un nuevo nodo
         Nodo* nodo = crear_nodo(sourceid, dstid, hod, mean_travel_time);
-        fwrite(nodo, sizeof(Nodo), 1, archivo_binario);
+
+        // Escribir el nodo en el archivo binario
+        fwrite(nodo, sizeof(Nodo), 1, binario);
+
+        // Liberar la memoria del nodo
         free(nodo);
     }
 
     // Cerrar los archivos
     fclose(archivo);
-    fclose(archivo_binario);
+    fclose(binario);
 
+    return 0;
+    }
+float search(int sourceid, int dstid, int hod) {
     // Abrir el archivo binario para lectura
-    archivo_binario = fopen("binario.bin", "rb");
-    if (archivo_binario == NULL) {
+    FILE* binario = fopen("binario.bin", "rb");
+    if (binario == NULL) {
         printf("Error al abrir el archivo binario\n");
         return 1;
     }
 
-    // Buscar una relación en el archivo binario
-    Nodo nodo;
-    int buscado_sourceid = 620, buscado_dstid = 12, buscado_hod = 21;
-    fseek(archivo_binario, 0, SEEK_SET);  // Mover el puntero al inicio del archivo
-    while (fread(&nodo, sizeof(Nodo), 1, archivo_binario)) {
-        if (nodo.sourceid == buscado_sourceid && nodo.dstid == buscado_dstid && nodo.hod == buscado_hod) {
-            printf("Tiempo promedio desde 1 a 2 a las 10am: %f\n", nodo.mean_travel_time);
-            break;
-        }
+    // Pedir al usuario que ingrese los valores de sourceid, dstid y hod
+
+    // buscar_L el nodo en el archivo binario
+    Nodo* nodo = buscar_binario(binario, sourceid, dstid, hod);
+    if (nodo == NULL) {
+        printf("NA\n");
+    } else {
+        printf("Mean travel time: %f\n", nodo->mean_travel_time);
     }
 
-    // Cerrar el archivo binario
-    fclose(archivo_binario);
+    // Cerrar el archivo
+    fclose(binario);
 
     return 0;
 }
